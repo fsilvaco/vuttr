716f6a61924d99790dae7872dae2cf313d27e3b1
Flavio Silva <contatoflaviodev@gmail.com>
2021-09-17 08:54:48 -0300

    style: tool item component

--- src/components/ToolItem/index.tsx
+++ src/components/ToolItem/index.tsx
@@ -10,8 +10,17 @@ interface ToolsProps {
 export const ToolItem = ({ tool }: ToolsProps) => {
   return (
     <ToolContainer>
-      <p>{tool.title}</p>
-      <p>{tool.description}</p>
+      <div className="tool-item">
+        <h3 className="tool-item__title">{tool.title}</h3>
+        <p className="tool-item__description">{tool.description}</p>
+        <ul className="tool-item__tags">
+          {tool.tags.map((tag, index) => (
+            <li className="tool-item__tags__item" key={index}>
+              #{tag}
+            </li>
+          ))}
+        </ul>
+      </div>
     </ToolContainer>
   );
 };
--- src/components/ToolItem/styles.ts
+++ src/components/ToolItem/styles.ts
@@ -2,7 +2,32 @@ import styled from "styled-components";
 
 export const ToolContainer = styled.div.attrs({ className: "tool-item" })`
   border: 2px solid #333;
+  padding: 1rem;
+
   &:not(:last-child) {
     margin-bottom: 2rem;
+  }
+  .tool-item {
+    &__title {
+      margin-bottom: 0.5rem;
+    }
+
+    &__description {
+      margin-bottom: 1rem;
+    }
+
+    &__tags {
+      margin: 0;
+
+      &__item {
+        display: inline;
+        list-style: none;
+        font-weight: bold;
+
+        &:not(:last-child) {
+          margin-right: 0.5rem;
+        }
+      }
+    }
   }
 `;
